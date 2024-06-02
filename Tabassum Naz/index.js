750f807de1c9842943c488ad0145bae7a4df08f8
TabassumNaz <155386325+TabassumNaz@users.noreply.github.com>
2024-06-02 19:45:07 +0500

    Add files via upload

---
+++ Tabassum Naz/index.js
@@ -0,0 +1,57 @@
+// New Work// 
+// Make 05 Variables// 
+
+let q1=("Is Java Script Case Sensitive?");
+let q2=("Does variable name contain any spaces?");
+let q3=("A part from string, can you assign numbers to the variable?");
+let q4=("Is Java Script a programming language");
+let q5=("Does variable name can contain only letters? ");
+
+// Make answers of the variables// 
+
+let a1=("yes");
+let a2=("no");
+let a3=("yes");
+let a4=("yes");
+let a5=("no");
+
+// Assign scores of 05 questios// 
+
+let score=0;
+let totalscore=5;
+
+// show alert in prompt section// 
+
+alert("Answert the given questions in Yes or No"); 
+
+// run prompt of 05 variables and put their answers // 
+
+let reply1=prompt(q1);
+let reply2=prompt(q2);
+let reply3=prompt(q3);
+let reply4=prompt(q4);
+let reply5=prompt(q5);
+
+//  assign conditions // 
+if (reply1="a1");
+{score++;}
+
+if (reply2="a2");
+{score++;}
+
+if (reply3="a3");
+{score++;}
+
+if (reply4="a4");
+{score++;}
+
+if (reply1="a1");
+{score++; }
+
+// assign formula to get result// 
+
+let percentage=score/totalscore*100
+
+alert(percentage); 
+
+
